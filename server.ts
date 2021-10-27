187c63a1bc0f1d840f628a2c2c75fd2dab2087cd
John Kuang <jkuang@gasbuddy.com>
2021-10-27 16:00:35 -0600

    for Heroku deployment

--- server.ts
+++ server.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 
 const app = express();
-const PORT = 8000;
-app.get('/quick-test', (req,res) => res.send('Express + TypeScript Server: being monitored'));
+const PORT = process.env.PORT || 8000;
+app.get('/quick-test', (req,res) => res.send('Express + TypeScript Server: deployed'));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
