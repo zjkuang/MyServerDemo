ecf47513596bb9c3c51b4180cddb52dc5de595ac
Zhengqian Kuang <dev.kuang@gmail.com>
2021-10-28 08:06:44 -0600

    fix node version conflict on local machine

--- server.ts
+++ server.ts
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 8000;
-app.get('/quick-test', (req,res) => res.send('Express + TypeScript Server: deployed'));
+app.get('/quick-test', (req,res) => res.send('[GET response from MyServerDemo] Express + TypeScript Server'));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
